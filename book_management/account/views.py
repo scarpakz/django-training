3f42d803cae1428086e5f6a449cf6e3fbe6dd3fb
scarpakz <jasonjjavier@gmail.com>
2019-08-16 17:58:32 +0800

    URL value in Ajax Changed

--- book_management/account/views.py
+++ book_management/account/views.py
@@ -17,7 +17,6 @@ from .serializers import BookSerializer
 class UserListView(ListView):
     model = User
 
-
 class UserLoginView(views.LoginView):
     template_name = 'account/login.html'
 
@@ -25,12 +24,10 @@ class UserLoginView(views.LoginView):
         url = reverse_lazy('dashboard')
         return url or resolve_url(settings.LOGIN_REDIRECT_URL)
 
-
 class UserCreateView(CreateView):
     template_name = 'account/register.html'
     form_class = SignUpForm
     success_url = reverse_lazy('login')
-
 
 @method_decorator(login_required, name='dispatch')
 class UserDashboardCreateView(ListView):
@@ -38,7 +35,6 @@ class UserDashboardCreateView(ListView):
     template_name = 'account/dashboard.html'
     success_url = reverse_lazy('dashboard')
     context_object_name = 'book'
-
 
 @method_decorator(login_required, name='dispatch')
 class UserDashboardDisplayUsers(ListView):
@@ -61,7 +57,6 @@ class BookCreateView(CreateView):
     form_class = BookForm
     success_url = reverse_lazy('dashboard')
 
-
 @method_decorator(login_required, name='dispatch')
 class BookUpdateView(UpdateView):
     form_class = BookForm
@@ -74,11 +69,7 @@ class BookCreateAPIView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+@method_decorator(login_required, name='dispatch')
 class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-# @method_decorator(login_required, name="dispatch")
-# class BookUpdateAPIView(generics.UpdateAPIView):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer

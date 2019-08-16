23ce1c4bd10d887e519a9b3de4447a016e8379db
scarpakz <jasonjjavier@gmail.com>
2019-08-16 10:10:52 +0800

    Add Image Functionality

--- book_management/account/models.py
+++ book_management/account/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Book(models.Model):
+    cover = models.ImageField(upload_to='images/', blank=True)
     name = models.CharField(max_length = 200)
     description = models.TextField()
     author = models.CharField(max_length = 100)
 
     def __str__(self):
-        return "{},{},{}".format(self.name, self.description, self.author)
+        return "{},{},{},{}".format(self.cover, self.name, self.description, self.author)
